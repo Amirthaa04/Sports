7f521ab2c2ee00efbbadd20da1bb1a4e31e88e5d
Amirthaa04 <amirthaswaathini.a2022cse@sece.ac.in>
2024-05-24 08:01:38 +0530

    hello

--- Schema3.js
+++ Schema3.js
@@ -26,34 +26,34 @@
 
 // module.exports = Cart;
 
-// const mongoose = require('mongoose')
-
-// const CartSchema = new mongoose.Schema({
-//     image: String,
-//     topic:String,
-//     description: String,
-//     price: String
-// });
-
-// const Cart = mongoose.model("Cart", CartSchema);
-// module.exports = Cart;
-
-// Schema3.js (or your Cart schema file)
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const CartSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Register',
-        required: true
-    },
-    topic: String,
+    image: String,
+    topic:String,
     description: String,
-    price: String,
-    image: String
+    price: String
 });
 
-const Cart = mongoose.model('Cart', CartSchema);
-
+const Cart = mongoose.model("Cart", CartSchema);
 module.exports = Cart;
+
+// Schema3.js (or your Cart schema file)
+// const mongoose = require('mongoose');
+
+// const CartSchema = new mongoose.Schema({
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Register',
+//         required: true
+//     },
+//     topic: String,
+//     description: String,
+//     price: String,
+//     image: String
+// });
+
+// const Cart = mongoose.model('Cart', CartSchema);
+
+// module.exports = Cart;
 
