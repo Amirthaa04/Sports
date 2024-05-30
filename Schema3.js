da4e42b115b9563a6f442e223c151e8d708935b3
Amirthaa04 <amirthaswaathini.a2022cse@sece.ac.in>
2024-05-30 12:31:06 +0530

    .

--- Schema3.js
+++ Schema3.js
@@ -11,35 +11,3 @@ const CartSchema = new mongoose.Schema({
 const Cart = mongoose.model('Cart', CartSchema);
 module.exports = Cart;
 
-
-// const mongoose = require('mongoose')
-
-// const CartSchema = new mongoose.Schema({
-//     image: String,
-//     topic:String,
-//     description: String,
-//     price: String
-// });
-
-// const Cart = mongoose.model("Cart", CartSchema);
-// module.exports = Cart;
-
-// Schema3.js (or your Cart schema file)
-// const mongoose = require('mongoose');
-
-// const CartSchema = new mongoose.Schema({
-//     userId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Register',
-//         required: true
-//     },
-//     topic: String,
-//     description: String,
-//     price: String,
-//     image: String
-// });
-
-// const Cart = mongoose.model('Cart', CartSchema);
-
-// module.exports = Cart;
-
